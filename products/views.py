9e75ab7f80bd13794abe371fef08f9e771a6f619
kavinsenthil21 <kavintsk21@gmail.com>
2020-06-24 14:02:20 +0530

    productds added

--- products/views.py
+++ products/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from .models import products
 # Create your views here.
 def home(request):
     return render(request,'products/home.html')
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image'] :
+            pr = products()
+            pr.title = request.POST['title']
+            pr.body = request.POST['body']
+            pr.url = request.POST['url']
+            pr.icon = request.FILES['icon']
+            pr.image = request.FILES['image']
+            pr.pubdate = timezone.datetime.now()
+            pr.hunter = request.user
+            pr.save()
+            return redirect('home')
+        
+        else:
+            return render(request,'products/create.html',{'error':'all fields are required'})
+
+    else:
+        return render(request,'products/create.html')
